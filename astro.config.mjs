ca1d0869925a54e12fc29fa98f1a1f1665ddfd75
nef734 <nefdevelop@gmail.com>
2026-02-13 21:44:10 +0100

    Blog update 2/13/2026 9:44pm: 4 file(s) changed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    server: {
+      watch: {
+        ignored: ['**/.obsidian/**', '**/_bases/**', '**/bases/**']
+      }
+    }
+  },
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
